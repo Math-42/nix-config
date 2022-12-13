d1cf46bdf54b34467abadd90bb2a36c29352389d
matheus <matheus.v4200@gmail.com>
2022-12-13 18:13:18 -0300

    nixos: add the base configuration file

---
+++ configuration.nix
@@ -0,0 +1,204 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./nvidia.nix
+      ./gnome.nix
+      ./build-tools.nix
+      ./languages/languages.nix
+      ./terminal/terminal.nix
+      ./miscellaneous/miscellaneous.nix
+      ./editors/editors.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "NN"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.utf8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.utf8";
+    LC_IDENTIFICATION = "pt_BR.utf8";
+    LC_MEASUREMENT = "pt_BR.utf8";
+    LC_MONETARY = "pt_BR.utf8";
+    LC_NAME = "pt_BR.utf8";
+    LC_NUMERIC = "pt_BR.utf8";
+    LC_PAPER = "pt_BR.utf8";
+    LC_TELEPHONE = "pt_BR.utf8";
+    LC_TIME = "pt_BR.utf8";
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "alt-intl";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  users.users.math-42 = {
+    isNormalUser = true;
+    description = "matheus";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    ];
+  };
+  
+  # Home manager configuration
+  #home-manager.users.math-42 = {pkgs, ...}: {
+  #  home.packages = [ pkgs.atool ];
+  #  programs.bash.enable = true;
+  #};
+
+  #home-manager.useUserPackages = true;
+  #home-manager.useGlobalPkgs = true;
+  
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  ############################################################################################################
+
+  # podman and distrobox
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+  };	
+
+  environment.systemPackages = with pkgs; [
+    #browsers 
+    google-chrome
+    firefox
+    qutebrowser
+
+    #libraries
+    # libvterm
+
+    binutils
+    coreutils
+    lld
+
+    #editors
+    # # emacs 
+    # ((emacsPackagesFor emacs).emacsWithPackages (epkgs: [
+    #   epkgs.vterm
+    # ]))
+
+    # emacs28Packages.vterm
+    
+    # Virtualization
+    distrobox
+    qemu
+
+    # Fonts
+    fira-mono
+    fira-code
+
+    #social
+    discord
+
+    # Media
+    stremio
+    vlc
+    zathura
+    qbittorrent
+    glow
+    steam
+    peek
+
+    # Productivity
+    obsidian
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  system.autoUpgrade.enable = true;
+  fonts.fonts = with pkgs; [
+  	(nerdfonts.override { fonts = ["FiraCode" "FiraMono"];})
+  ];
+
+  nix.settings.auto-optimise-store = true;
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+  programs.steam.enable = true;
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  ############################################################################################################
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+
+}
